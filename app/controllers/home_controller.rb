de64ef6c76ae5b4ca5034f613958eae24fb7a563
sjky1204 <sjky1204@likelion.org>
2017-05-11 20:55:59 +0900

    최종 완료

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  	q = Question.new
+  	q.name = params[:db_name]
+  	q.content = params[:db_content]
+  	q.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  get '/msv' => "home#index"
+  post 'home/create'
+  root 'home#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

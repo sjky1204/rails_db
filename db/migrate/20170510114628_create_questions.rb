c71e661835f65772f967fdc1efe149d49542f718
sjky1204 <sjky1204@likelion.org>
2017-05-11 19:12:38 +0900

    db+

--- db/migrate/20170510114628_create_questions.rb
+++ db/migrate/20170510114628_create_questions.rb
@@ -1,7 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration[5.0]
   def change
     create_table :questions do |t|
-
+    	t.string :name
+    	t.string :content
       t.timestamps
     end
   end
